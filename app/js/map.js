84520ce54c4f8180373c62a6e5e88981f483d3c8
Salvador Guzman <guzmansalv@gmail.com>
2017-01-30 19:15:20 -0800

    added layer group overlay

--- app/js/map.js
+++ app/js/map.js
@@ -1,9 +1,19 @@
 /**
  * Created by salvadorguzman on 1/30/17.
  */
-
 let tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
 });
 
-let map = L.map('map', {layers: [tiles]}).setView([34.430033, -119.868154], 12);
+let map = L.map('map', {layers: [tiles]}).setView([34.430033, -119.868154], 13);
+let layer = L.markerClusterGroup();
+
+let baseLayers = {
+    "OpenStreetMaps": tiles,
+};
+
+let overlays = {
+    "Churches": layer
+};
+
+L.control.layers(baseLayers, overlays).addTo(map);
